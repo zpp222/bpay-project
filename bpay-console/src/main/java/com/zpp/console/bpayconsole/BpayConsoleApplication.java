60f16d6779e6f3b345e979f4aa28f6951488bd82
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-05-20 15:36:35 +0800

    Add files via upload

---
+++ bpay-console/src/main/java/com/zpp/console/bpayconsole/BpayConsoleApplication.java
@@ -0,0 +1,12 @@
+package com.zpp.console.bpayconsole;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpayConsoleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpayConsoleApplication.class, args);
+	}
+}
---
+++ bpay-service/src/main/java/com/zpp/service/bpayservice/BpayServiceApplication.java
@@ -0,0 +1,12 @@
+package com.zpp.service.bpayservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BpayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BpayServiceApplication.class, args);
+	}
+}
