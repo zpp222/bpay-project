ce5fcd84fbf7d8bb8111a966b93e64a221e56abe
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-05-22 20:40:00 +0800

    add discover

--- bpay-console/src/main/java/com/zpp/console/bpayconsole/BpayConsoleApplication.java
+++ bpay-console/src/main/java/com/zpp/console/bpayconsole/BpayConsoleApplication.java
@@ -2,7 +2,9 @@ package com.zpp.console.bpayconsole;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class BpayConsoleApplication {
 
