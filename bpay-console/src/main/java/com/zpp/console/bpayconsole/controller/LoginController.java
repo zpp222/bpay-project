64df861adf81f9ad86df1d75ebe87a7608f23691
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-05-22 11:27:29 +0800

    update

--- bpay-console/src/main/java/com/zpp/console/bpayconsole/controller/LoginController.java
+++ bpay-console/src/main/java/com/zpp/console/bpayconsole/controller/LoginController.java
@@ -1,11 +1,9 @@
 package com.zpp.console.bpayconsole.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@EnableAutoConfiguration
 public class LoginController {
 
 	@RequestMapping("/")
