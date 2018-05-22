00edb915c775cf338fecf96d972b7764a00acf76
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-05-22 12:44:01 +0800

    update

---
+++ bpay-service/src/main/java/com/zpp/service/bpayservice/dto/User.java
@@ -0,0 +1,34 @@
+package com.zpp.service.bpayservice.dto;
+
+public class User {
+
+	private String id;
+	private String sex;
+	private String phone;
+	private String name;
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public String getPhone() {
+		return phone;
+	}
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+}
---
+++ bpay-service/src/main/java/com/zpp/service/bpayservice/mapper/UserMapper.java
@@ -0,0 +1,9 @@
+package com.zpp.service.bpayservice.mapper;
+
+import com.zpp.service.bpayservice.dto.User;
+
+public interface UserMapper {
+
+	public User select(String name);
+
+}
