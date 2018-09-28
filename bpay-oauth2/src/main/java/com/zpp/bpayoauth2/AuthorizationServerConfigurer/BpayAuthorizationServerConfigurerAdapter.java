5569e9ee2ed19a9f4c0d8bfd0707407418a4ea27
zpp <zhangpengp993@163.com>
2018-09-28 11:57:21 +0800

    up

--- bpay-oauth2/src/main/java/com/zpp/bpayoauth2/AuthorizationServerConfigurer/BpayAuthorizationServerConfigurerAdapter.java
+++ bpay-oauth2/src/main/java/com/zpp/bpayoauth2/AuthorizationServerConfigurer/BpayAuthorizationServerConfigurerAdapter.java
@@ -1,31 +1,78 @@
 package com.zpp.bpayoauth2.AuthorizationServerConfigurer;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerSecurityConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
+import org.springframework.security.oauth2.provider.token.store.InMemoryTokenStore;
 
 @Configuration
+@EnableAuthorizationServer
 public class BpayAuthorizationServerConfigurerAdapter extends AuthorizationServerConfigurerAdapter {
 
+	private TokenStore tokenStore = new InMemoryTokenStore();
+
+	@Autowired
+	@Qualifier("authenticationManagerBean")
+	private AuthenticationManager authenticationManager;
+
+	/**
+	 * 授权和token的服务
+	 */
 	@Override
 	public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
-		super.configure(endpoints);
-		System.out.println("\n######################\n");
+		endpoints.tokenStore(tokenStore).authenticationManager(authenticationManager);
 	}
 
+	/**
+	 * 访问token接口的安全限制
+	 */
 	@Override
 	public void configure(AuthorizationServerSecurityConfigurer security) throws Exception {
 		security.tokenKeyAccess("permitAll()");
 		security.checkTokenAccess("isAuthenticated()");
-		security.allowFormAuthenticationForClients();
 	}
 
+	/**
+	 * 客户端连接详情
+	 */
 	@Override
 	public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
-		clients.inMemory().withClient("client").secret("secret").scopes("app")
-				.authorizedGrantTypes("authorization_code", "password", "refresh_token");
+		clients.inMemory().withClient("client").secret("secret")
+				.authorizedGrantTypes("authorization_code", "refresh_token", "password").scopes("openid");
+	}
+
+	@Configuration
+	@EnableWebSecurity
+	protected static class webSecurityConfig extends WebSecurityConfigurerAdapter {
+
+		@Override
+		protected void configure(HttpSecurity http) throws Exception {
+			http.authorizeRequests().anyRequest().authenticated().and().csrf().disable().formLogin();;
+		}
+
+		@Override
+		protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+			auth.inMemoryAuthentication().passwordEncoder(new BCryptPasswordEncoder());
+		}
+
+		@Override
+		@Bean
+		public AuthenticationManager authenticationManagerBean() throws Exception {
+			return super.authenticationManagerBean();
+		}
 	}
 
 }
