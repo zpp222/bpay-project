a80219e000bf1e38dde82421563848ecff33ca11
zpp <zhangpengp993@163.com>
2018-10-16 15:23:43 +0800

    update

--- bpay-oauth2/src/main/java/com/zpp/bpayoauth2/BpayOauth2Application.java
+++ bpay-oauth2/src/main/java/com/zpp/bpayoauth2/BpayOauth2Application.java
@@ -2,8 +2,10 @@ package com.zpp.bpayoauth2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class BpayOauth2Application {
 
 	public static void main(String[] args) {
