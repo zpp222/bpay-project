18fef06dcca63a49198ed3894d59d4e25dae082f
zpp <zhangpengp993@163.com>
2018-10-19 09:43:41 +0800

    up

--- bpay-oauth2/src/main/java/com/zpp/bpayoauth2/securityConfig/BpaySecurityConfig.java
+++ bpay-oauth2/src/main/java/com/zpp/bpayoauth2/securityConfig/BpaySecurityConfig.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +15,6 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-@Order(1)
 public class BpaySecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
