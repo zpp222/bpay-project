4e949f629d9c6db1aa2a5e765abfed529218c8dd
zpp <zhangpengp993@163.com>
2018-10-19 09:43:41 +0800

    up

--- bpay-oauth2/src/main/java/com/zpp/bpayoauth2/resource/BpayResourceServerConfiguration.java
+++ bpay-oauth2/src/main/java/com/zpp/bpayoauth2/resource/BpayResourceServerConfiguration.java
@@ -1,15 +1,17 @@
 package com.zpp.bpayoauth2.resource;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 
 @Configuration
 @EnableResourceServer
+@Order(3)
 public class BpayResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 	@Override
 	public void configure(HttpSecurity http) throws Exception {
-		http.antMatcher("/auth/**").authorizeRequests().anyRequest().authenticated();
+		http.antMatcher("/open/**").authorizeRequests().anyRequest().authenticated();
 	}
 }
