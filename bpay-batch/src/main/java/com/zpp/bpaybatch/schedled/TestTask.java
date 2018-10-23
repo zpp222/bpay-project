2467e90641294af3ae939ec019cdfcb0a51d4671
zpp222 <38618638+zpp222@users.noreply.github.com>
2018-10-23 23:30:24 +0800

    up

--- bpay-batch/src/main/java/com/zpp/bpaybatch/schedled/TestTask.java
+++ bpay-batch/src/main/java/com/zpp/bpaybatch/schedled/TestTask.java
@@ -1,11 +1,15 @@
 package com.zpp.bpaybatch.schedled;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.quartz.JobDataMap;
 import org.quartz.JobDetail;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameter;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.configuration.JobLocator;
 import org.springframework.batch.core.launch.JobLauncher;
@@ -24,7 +28,11 @@ public class TestTask extends QuartzJobBean {
 		JobLocator jobLocator = (JobLocator) SpringContextUtil.getBean(JobLocator.class);
 		try {
 			Job job = jobLocator.getJob(jobName);
-			JobExecution jobExecution = jobLauncher.run(job, new JobParameters());
+			// 批量任务执行参数，区分是否为同一个任务（执行一次）
+			Map<String, JobParameter> map = new HashMap<>();
+			JobParameter currentTimeMillis = new JobParameter(System.currentTimeMillis());
+			map.put("currentTimeMillis", currentTimeMillis);
+			JobExecution jobExecution = jobLauncher.run(job, new JobParameters(map));
 			jobExecution.getExitStatus();
 		} catch (Exception e) {
 			e.printStackTrace();
