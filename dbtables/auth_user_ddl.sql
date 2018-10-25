863264c04eea3e461934c0b6dd8883ab5af89cf8
zpp <zhangpengp993@163.com>
2018-10-25 08:55:00 +0800

    delete

--- dbtables/auth_user_ddl.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user` (
-  `id` varchar(5) DEFAULT NULL,
-  `name` varchar(20) DEFAULT NULL,
-  `phone` varchar(15) DEFAULT NULL,
-  `sex` varchar(5) DEFAULT NULL,
-  `passwd` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8
