4c27ab4f4b2fa23fa5fa26297863149c11b529bf
zpp <zhangpengp993@163.com>
2018-10-25 15:18:27 +0800

    优化sso：1、client for sso-client；2、zuul for sso-resource

--- README.md
+++ README.md
@@ -1,28 +1,29 @@
 # bpay-project
 
-## spring boot/cloud
-> 1.dubbo for service and console.
-> 2.eureka for console and client with eureka.
-> 3.Hystrix for client
+>## spring boot/cloud
+### 1.dubbo for service and console.
+### 2.eureka for console and client with eureka.
+### 3.Hystrix for client
 
-## db
-> 1.mysql relation db for service.
-> 2.mongodb document store for store.
-> 3.redis\hezalcast key/value for service.
+>## db
+### 1.mysql relation db for service.
+### 2.mongodb document store for store.
+### 3.redis\hezalcast key/value for service.
 
-## Messaging
-> 1. kafka for store with service.
+>## Messaging
+### 1. kafka for store with service.
 
-## Cloud Routing
-> 1.Spring Cloud Gateway(NIO)
-> 2.zuul
+>## Cloud Routing
+### 1.Spring Cloud Gateway(NIO)
+### 2.zuul
 
-## OAuth2
-> 1.OAuth2 and client
-> 2.zuul api
+>## OAuth2
+### 1.OAuth2 for sso-provider 
+### 2.client for sso-client
+### 2.zuul for sso-resource
 
-## batch+quartz
-> 1. bpay-batch for batch job.
+>## batch+quartz
+### 1. bpay-batch for batch job.
 
 ## 架构图
 ```mermaid
--- bpay-client/src/main/java/com/zpp/bpayclient/sso/SecurityConfig.java
+++ bpay-client/src/main/java/com/zpp/bpayclient/sso/SecurityConfig.java
@@ -1,17 +1,18 @@
 package com.zpp.bpayclient.sso;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 
 @Configuration
 @EnableOAuth2Sso
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().permitAll();
-		http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+public class SecurityConfig {
+	
+	@Bean("oauth")
+	public OAuth2RestTemplate restTemplate(UserInfoRestTemplateFactory factory) {
+		return factory.getUserInfoRestTemplate();
 	}
+
 }
