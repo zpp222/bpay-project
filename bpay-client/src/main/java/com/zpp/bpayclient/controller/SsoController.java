6920314098e8674fcd60a4c930a00177c5261b6a
zpp222 <zhangpengp993@163.com>
2018-11-04 19:00:33 +0800

    add config-server

--- bpay-client/src/main/java/com/zpp/bpayclient/controller/SsoController.java
+++ bpay-client/src/main/java/com/zpp/bpayclient/controller/SsoController.java
@@ -1,17 +1,21 @@
 package com.zpp.bpayclient.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SsoController {
-	
+
+	@Value("${info.profile}")
+	private String info;
+
 	@GetMapping("/test")
 	@PreAuthorize("hasAnyRole('ROLE_USER')")
 	public String test() {
 		System.out.println("#client test...");
-		return "test";
+		return "test " + info;
 	}
 
 	@GetMapping("/test2")
