12414aa65f2c27e59c724dd7b158c0d1864cfa7f
zpp222 <zhangpengp993@163.com>
2018-11-05 22:19:37 +0800

    update

--- bpay-config-server/src/main/java/com/zpp/bpayconfigserver/BpayConfigServerApplication.java
+++ bpay-config-server/src/main/java/com/zpp/bpayconfigserver/BpayConfigServerApplication.java
@@ -2,12 +2,10 @@ package com.zpp.bpayconfigserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-@EnableOAuth2Sso
 public class BpayConfigServerApplication {
 
 	public static void main(String[] args) {
--- bpay-config-server/src/main/java/com/zpp/bpayconfigserver/sso/SecurityConfig.java
+++ bpay-config-server/src/main/java/com/zpp/bpayconfigserver/sso/SecurityConfig.java
@@ -1,21 +1,18 @@
 package com.zpp.bpayconfigserver.sso;
 
-//import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
-@Order(1)
+@EnableOAuth2Sso
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		http.formLogin().permitAll();
-		http.antMatcher("/actuator/**").authorizeRequests().anyRequest().hasRole("ENDPOINT_ADMIN");
-		http.antMatcher("/bpay**/**").authorizeRequests().anyRequest().permitAll();
-//		http.requestMatcher(EndpointRequest.toAnyEndpoint()).authorizeRequests().anyRequest().hasRole("ENDPOINT_ADMIN");
+		http.csrf().disable();
+		http.authorizeRequests().antMatchers("/actuator/**").hasRole("ENDPOINT_ADMIN");
 	}
 
 }
